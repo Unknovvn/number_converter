d07bdd610fa76e0020bae958649eda79dc5baa58
Andzej Korovacki <Andrzej.Korovacki@gmail.com>
2021-10-02 21:27:24 +0300

    to_lithuanian overloaded with Int32 and shard.yml fixed

--- src/number_converter.cr
+++ src/number_converter.cr
@@ -6,4 +6,8 @@ module NumberConverter
   def to_lithuanian(number : Int64) : String
     return Lithuanian.convert_number(number)
   end
+
+  def to_lithuanian(number : Int32) : String
+    return Lithuanian.convert_number(number.to_i64)
+  end
 end
